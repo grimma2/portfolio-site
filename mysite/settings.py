257c08e196598f2d5751317478595351f08a9573
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-09-24 13:58:07 +0500

    turn off DEBUG

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)4j7*87)pa1ttg6gn_o&1zi8sz3l*-7m07q4qvqn4n4zugcgnv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-site-production-43ee.up.railway.app']
 
 
 # Application definition
